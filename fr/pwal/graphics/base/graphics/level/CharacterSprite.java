28b9ad9f4fa21aeaabc053c71a8d93af972154a3
Gagoi <Gagoi@Gagoi-PC>
2016-04-06 22:02:16 +0200

    Remove a debug information.

--- fr/pwal/graphics/base/graphics/level/CharacterSprite.java
+++ fr/pwal/graphics/base/graphics/level/CharacterSprite.java
@@ -26,7 +26,6 @@ public class CharacterSprite {
 		for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 3; j++){
 				this.texture[i * 3 + j] = spriteSheet.getSubimage(j*16, i*16, 16, 16);
-				System.out.println(i*3+j);
 			}
 
 	
